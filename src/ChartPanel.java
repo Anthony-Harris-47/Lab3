5caea580de43b1322be3debdabb7f4289c8762f6
RipRed7 <aharris47@cub.uca.edu>
2024-10-28 06:52:27 -0500

    init commit

---
+++ src/ChartPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ChartPanel extends JPanel {
+
+
+    public void setConfig() {
+        this.setPreferredSize(new Dimension(50,200));
+        this.setBackground(Color.black);
+        this.setVisible(true);
+    }
+}
